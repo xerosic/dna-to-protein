be6f404c2b3f1bb84bf85cde96a63c80b8d8f76a
xerosic <42121005+xerosic@users.noreply.github.com>
2021-11-12 11:22:07 +0100

    Fixed funny test

--- rna_translation/rna_translation.cpp
+++ rna_translation/rna_translation.cpp
@@ -5,7 +5,7 @@ int main()
     std::string exon;
     std::vector<std::string> codon;
 
-    std::cout << "Super omega roby divisore di esoni 2000" << std::endl << "Inserisci l'esone maturo come la prozia di Reus: ";
+    std::cout << "Super omega roby divisore di esoni 2000" << std::endl << "Inserisci l'esone maturo: ";
     std::cin >> exon;
 
     codon = split_exon(exon);
